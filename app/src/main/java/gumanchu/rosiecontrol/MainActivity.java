b0f5f8c0c1e8d032cf578da48818d4ab29a643f3
Freddy <freddy.gu@gmail.com>
2015-06-02 22:40:59 -0400

    Added preliminary Cardboard code.  Can stream and present camera

--- app/src/main/java/gumanchu/rosiecontrol/MainActivity.java
+++ app/src/main/java/gumanchu/rosiecontrol/MainActivity.java
@@ -2,10 +2,14 @@ package gumanchu.rosiecontrol;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,7 +18,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 import com.google.vrtoolkit.cardboard.CardboardActivity;
 import com.google.vrtoolkit.cardboard.CardboardView;
@@ -36,11 +39,17 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 import java.text.DecimalFormat;
 
 import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
-public class MainActivity extends CardboardActivity implements SensorEventListener, CardboardView.StereoRenderer {
+@SuppressWarnings("deprecation")
+public class MainActivity extends CardboardActivity implements SensorEventListener, CardboardView.StereoRenderer, SurfaceTexture.OnFrameAvailableListener {
 
     private static final String TAG = "RosieControl";
 
@@ -52,7 +61,7 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
     /*
      * Vars for VideoTask
      */
-    private ImageView imView;
+//    private ImageView imView;
     Mat img, tmp, ret;
     Bitmap bm;
     long imgSize;
@@ -63,8 +72,9 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
     // Used for ControlTask
     int currentKey, previousKey;
     ControlRunnable controller;
+    RosieTask streamer;
 
-    // Sensor vars. TODO: Potentially replace with Cardboard SDK
+    // Sensor vars.
     private SensorManager senManager;
     private Sensor accel;
     private Sensor mag;
@@ -76,10 +86,66 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
     DataInputStream in;
     DataOutputStream out;
 
+
+    //TODO: START OF CARD?BOARD VARS
+
+    private static final int GL_TEXTURE_EXTERNAL_OES = 0x8D65;
     CardboardView cardboardView;
-    private CardboardOverlayView overlayView;
-    private float[] camera;
-    private float[] view;
+    private SurfaceTexture surface;
+    int mProgram;
+    CardboardOverlayView overlayView;
+    float[] camera;
+    float[] view;
+
+    private FloatBuffer vertexBuffer, textureVerticesBuffer;
+    private ShortBuffer drawListBuffer;
+    int texture;
+    int mPositionHandle;
+    int mColorHandle;
+    int mTextureCoordHandle;
+    static final int COORDS_PER_VERTEX = 2;
+    final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+    Camera cam;
+
+
+
+    static float squareVertices[] = { // in counterclockwise order:
+            -1.0f, -1.0f,   // 0.left - mid
+            1.0f, -1.0f,   // 1. right - mid
+            -1.0f, 1.0f,   // 2. left - top
+            1.0f, 1.0f,   // 3. right - top
+    };
+
+    static float textureVertices[] = {
+            0.0f, 1.0f,  // A. left-bottom
+            1.0f, 1.0f,  // B. right-bottom
+            0.0f, 0.0f,  // C. left-top
+            1.0f, 0.0f   // D. right-top
+    };
+
+    private short drawOrder[] = {0, 2, 1, 1, 2, 3}; // order to draw vertices
+
+    final String vertexShaderCode =
+            "attribute vec4 position;" +
+                    "attribute vec2 inputTextureCoordinate;" +
+                    "varying vec2 textureCoordinate;" +
+                    "void main()" +
+                    "{" +
+                    "gl_Position = position;" +
+                    "textureCoordinate = inputTextureCoordinate;" +
+                    "}";
+
+    final String fragmentShaderCode =
+            "#extension GL_OES_EGL_image_external : require\n" +
+                    "precision mediump float;" +
+                    "varying vec2 textureCoordinate;                            \n" +
+                    "uniform samplerExternalOES s_texture;               \n" +
+                    "void main(void) {" +
+                    "  gl_FragColor = texture2D( s_texture, textureCoordinate );\n" +
+                    //"  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n" +
+                    "}";
+
+    //TODO: END OF CARDBOARD VARS
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -133,7 +199,7 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
         setCardboardView(cardboardView);
 
         overlayView = (CardboardOverlayView) findViewById(R.id.overlay);
-        overlayView.show3DToast("Hi mum");
+        overlayView.show3DToast("Hi dad");
 
         camera = new float[16];
         view = new float[16];
@@ -164,26 +230,116 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
 
         bm = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
 
-//        new RosieTask().execute();
+
+
+        streamer = new RosieTask();
+//        streamer.execute();
 //
-//        controller = new ControlRunnable();
+        controller = new ControlRunnable();
 //        Thread t = new Thread(controller);
 //        t.start();
     }
 
+
+    //TODO: POINTER FOR START OF TEMPORARY CARDBOARD VIEWER
+
+
+    private int loadGLShader(int type, String code) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, code);
+        GLES20.glCompileShader(shader);
+
+        // Get the compilation status.
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0) {
+            Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+            GLES20.glDeleteShader(shader);
+            shader = 0;
+        }
+
+        if (shader == 0) {
+            throw new RuntimeException("Error creating shader.");
+        }
+
+        return shader;
+    }
+
+    static private int createTexture() {
+        int[] texture = new int[1];
+
+        GLES20.glGenTextures(1, texture, 0);
+        GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, texture[0]);
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+
+        return texture[0];
+    }
+
+    public void startCamera(int texture) {
+        surface = new SurfaceTexture(texture);
+        surface.setOnFrameAvailableListener(this);
+
+        cam = Camera.open();
+
+        try {
+            cam.setPreviewTexture(surface);
+            cam.startPreview();
+        } catch (IOException ioe) {
+            Log.w(TAG, "CAM LAUNCH FAILED");
+        }
+    }
+
+
     @Override
-    public void onNewFrame(HeadTransform headTransform) {
+    public void onSurfaceCreated(EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 0.5f); // Dark background so text shows up well
+
+        ByteBuffer bb = ByteBuffer.allocateDirect(squareVertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(squareVertices);
+        vertexBuffer.position(0);
+
+        ByteBuffer bb2 = ByteBuffer.allocateDirect(textureVertices.length * 4);
+        bb2.order(ByteOrder.nativeOrder());
+        textureVerticesBuffer = bb2.asFloatBuffer();
+        textureVerticesBuffer.put(textureVertices);
+        textureVerticesBuffer.position(0);
+
+        ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
+
+        int vertexShader = loadGLShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = loadGLShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        mProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        GLES20.glLinkProgram(mProgram);
+
+        texture = createTexture();
+        startCamera(texture);
+
+
+    }
+
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
 
     }
 
     @Override
-    public void onDrawEye(Eye eye) {
-
-    }
-
-    @Override
-    public void onRendererShutdown() {
-
+    public void onFrameAvailable(SurfaceTexture arg0) {
+        this.cardboardView.requestRender();
     }
 
     @Override
@@ -192,15 +348,50 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
     }
 
     @Override
-    public void onSurfaceCreated(EGLConfig config) {
+    public void onNewFrame(HeadTransform headTransform) {
+
+        float[] mtx = new float[16];
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        surface.updateTexImage();
+        surface.getTransformMatrix(mtx);
 
     }
 
     @Override
-    public void onSurfaceChanged(int width, int height) {
+    public void onDrawEye(Eye eye) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        GLES20.glUseProgram(mProgram);
+
+        GLES20.glActiveTexture(GL_TEXTURE_EXTERNAL_OES);
+        GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, texture);
+
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "position");
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+
+        mTextureCoordHandle = GLES20.glGetAttribLocation(mProgram, "inputTextureCoordinate");
+        GLES20.glEnableVertexAttribArray(mTextureCoordHandle);
+        GLES20.glVertexAttribPointer(mTextureCoordHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, textureVerticesBuffer);
+
+        mColorHandle = GLES20.glGetAttribLocation(mProgram, "s_texture");
+
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glDisableVertexAttribArray(mTextureCoordHandle);
+
+        Matrix.multiplyMM(view, 0, eye.getEyeView(), 0, camera, 0);
+
 
     }
 
+    @Override
+    public void onRendererShutdown() {
+        Log.i(TAG, "onRendererShutdown");
+    }
+
+    //TODO: END OF TEMPORARY POINTER
 
 
     @Override
@@ -222,7 +413,7 @@ public class MainActivity extends CardboardActivity implements SensorEventListen
                 orientationD[0] = (float) Math.toDegrees(orientation[0]);
                 orientationD[1] = (float) Math.toDegrees(orientation[1]);
                 orientationD[2] = (float) Math.toDegrees(orientation[2]);
-//                Log.i(TAG, "X: " + dFormat.format(orientationD[1]) + ", Y: " + dFormat.format(orientationD[2]) + ", Z: " + dFormat.format(orientationD[0]));
+                Log.i(TAG, "X: " + dFormat.format(orientationD[1]) + ", Y: " + dFormat.format(orientationD[2]) + ", Z: " + dFormat.format(orientationD[0]));
             }
         }
 
